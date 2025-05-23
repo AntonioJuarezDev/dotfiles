209af2ee41a7eb023873d7b20364a9d850305433
Antonio <juarez.antonio156@protonmail.com>
2025-05-22 23:15:27 -0600

    added brackets to hostname

--- prompt_min_v2.sh
+++ prompt_min_v2.sh
@@ -2,6 +2,7 @@
 # Custom PS1
 
 HOSTN_FG=36
+HOSTN_BRKT_FG=90
 GIT_FG=35
 DIR_FG=33
 PROMPT_FG=35
@@ -14,7 +15,7 @@ git_branch() {
 }
 
 CHECK="\$([[ \$? == 0 ]] && echo -e \[\033[32m\]❙ || echo -e \[\033[31m\]❙) "
-HOSTN="\[\033[1;${HOSTN_FG}m\]\$(hostname) "
+HOSTN="\[\033[1;${HOSTN_BRKT_FG}m\]〔\[\033[1;${HOSTN_FG}m\]\$(hostname)\[\033[1;${HOSTN_BRKT_FG}m\]〕 "
 GIT_BRANCH="\[\033[1;${GIT_FG}m\]\$(git_branch)"
 CURRENT_DIR="\[\033[1;${DIR_FG}m\]\w\n"
 #P_PROMPT="\[\033[0;${P_PROMPT_FG}m\]➤\[\033[0;39;49m\] "
